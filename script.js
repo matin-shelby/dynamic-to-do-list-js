2638072932e07b112d409cf3bce5a01b7f8d1623
Matin <olayemiolamilekan05@gmail.com>
2025-08-28 21:33:01 +0100

    Implement To-Do app task logic

---
+++ script.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const addButton = document.getElementById('add-task-btn');
+    const taskInput = document.getElementById('task-input');
+    const taskList = document.getElementById('task-list');
+
+    addButton.addEventListener('click', addTask);
+    taskInput.addEventListener('keypress', () => {
+        if (event.key === 'Enter')
+            addTask();
+    })
+
+    // add task function
+function addTask() {
+    const taskText = taskInput.value.trim();
+    if (taskText === "")
+        alert("Pls Enter a Task.");
+    else {
+        const task = document.createElement('li');
+        task.textContent = taskText;
+
+        // remove task logic
+        const removeTask = document.createElement('button');
+        removeTask.textContent = "Remove";
+        removeTask.className = "remove-btn";
+        removeTask.addEventListener('click', () => {
+            taskList.removeChild(task);
+        })
+        task.appendChild(removeTask);
+        taskList.appendChild(task);
+        taskInput.value = "";
+    }
+}
+})
+
+document.addEventListener('DOMContentLoaded', addTask);
+
